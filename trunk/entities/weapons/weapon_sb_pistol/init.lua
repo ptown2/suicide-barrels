d5f4a6f2c08d6abc3b79550e645d229c5abfcf7c
xptown2x@gmail.com <xptown2x@gmail.com@c08a3462-cb08-bb93-16b6-1bae57b65a39>
2013-07-24 18:29:24 +0000

    Added weapon_sb_pistol

---
+++ trunk/entities/weapons/weapon_sb_pistol/init.lua
@@ -0,0 +1,11 @@
+AddCSLuaFile( "cl_init.lua")
+AddCSLuaFile( "shared.lua")
+
+include( "shared.lua" )
+
+function SWEP:Think()
+	if self.IdleAnimation and self.IdleAnimation <= CurTime() then
+		self.IdleAnimation = nil
+		self:SendWeaponAnim( ACT_VM_IDLE )
+	end
+end
---
+++ trunk/entities/weapons/weapon_sb_pistol/shared.lua
@@ -0,0 +1,100 @@
+SWEP.PrintName			= "Pistol"
+
+SWEP.DrawCrosshair		= true
+SWEP.DrawAmmo			= true
+
+SWEP.ViewModelFOV		= 70
+SWEP.ViewModel			= "models/weapons/v_pistol.mdl"
+SWEP.WorldModel			= "models/weapons/w_pistol.mdl"
+
+SWEP.AutoSwitchTo		= true
+SWEP.AutoSwitchFrom		= false
+
+SWEP.Slot				= 0
+SWEP.SlotPos			= 1
+SWEP.Weight				= 5
+SWEP.HoldType			= "pistol"
+
+SWEP.ShootSound			= Sound( "Weapon_Pistol.Single" )
+SWEP.ReloadSound		= Sound( "Weapon_Pistol.Reload" )
+SWEP.EmptySound			= Sound( "Weapon_Pistol.Empty" )
+
+SWEP.Primary.Damage		= 9999
+SWEP.Primary.ClipSize	= 1
+SWEP.Primary.Shots		= 1
+SWEP.Primary.Spread		= 0.25
+SWEP.Primary.Delay		= 0.35
+
+-- Meta Functions
+function SWEP:GetNextReload()
+	return self.m_NextReload || 0
+end
+
+function SWEP:SetNextReload( fTime )
+	self.m_NextReload = CurTime() + fTime
+end
+
+
+function SWEP:Initialize()
+	self:SetWeaponHoldType( self.HoldType )
+	self:SetDeploySpeed( 1.1 )
+end
+
+function SWEP:Deploy()
+	self:SetNextReload( 0 )
+	self.IdleAnimation = CurTime() + self:SequenceDuration()
+
+	return true
+end
+
+function SWEP:Reload()
+	--if ( self:Clip1() >= 1 ) then return end
+
+	if ( self:GetNextReload() <= CurTime() ) && self:DefaultReload( ACT_VM_RELOAD ) then
+		self.IdleAnimation = CurTime() + self:SequenceDuration()
+		self:SetNextReload( self:SequenceDuration() )
+
+		self:EmitSound( self.ReloadSound )
+		self.Owner:DoReloadEvent()
+		self:SendWeaponAnim( ACT_VM_RELOAD )
+	end
+end
+
+function SWEP:CanPrimaryAttack()
+	if ( self:Clip1() <= 0 ) then
+		self:EmitSound( self.EmptySound )
+		self:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
+
+		return false
+	end
+
+	return true
+end
+
+function SWEP:PrimaryAttack()
+	if ( !self:CanPrimaryAttack() ) then return end
+	self:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
+	self.IdleAnimation = CurTime() + self:SequenceDuration()
+
+	local bullet = {}
+	bullet.Num			= self.Primary.Shots
+	bullet.Src			= self.Owner:GetShootPos()
+	bullet.Dir			= self.Owner:GetAimVector()
+	bullet.Spread		= Vector( self.Primary.Spread * 0.1 , self.Primary.Spread * 0.1, 0)
+	bullet.Damage		= self.Primary.Damage
+	bullet.AmmoType		= self.Primary.Ammo
+
+	self:TakePrimaryAmmo( self.Primary.Shots )
+	self:EmitSound( self.ShootSound )
+	self.Owner:FireBullets( bullet )
+	self.Owner:DoAttackEvent()
+	self:SendWeaponAnim( ACT_VM_PRIMARYATTACK )
+end
+
+
+function SWEP:CanSecondaryAttack()
+	return false
+end
+
+function SWEP:SecondaryAttack()
+end
