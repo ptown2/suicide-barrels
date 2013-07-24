c31f529211357c77f6609fa6ffa8a8e63dd0138f
xptown2x@gmail.com <xptown2x@gmail.com@c08a3462-cb08-bb93-16b6-1bae57b65a39>
2013-07-24 14:50:15 +0000

    Added the GNU License, redid a bit the sh_loading.lua and edited the base a little.

--- trunk/gamemode/init.lua
+++ trunk/gamemode/init.lua
@@ -1,4 +1,48 @@
+/*
+	Suicide Barrels - GM13 Edition
+
+	Redone/Recoded by: ptown2 (Robert Lind)
+	Original Authors:
+		*
+		*
+		*
+*/
+
+/* --------------------------------------------------------------------------
+    Suicide Barrels GM13 Edition
+    Copyright (C) 2013  Robert Lind (ptown2) and David Marcec (ogniK)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-------------------------------------------------------------------------- */
+
 AddCSLuaFile("cl_init.lua")
+
 AddCSLuaFile("shared.lua")
 AddCSLuaFile("sh_loading.lua")
-include("shared.lua")
+
+include( "shared.lua" )
+
+function GM:ManageNetworks()
+	--util.AddNetworkString( "" )
+end
+
+function GM:Initialize()
+	self:PrecacheResources()
+	self:ManageNetworks()
+	self:LoadModules()
+end
+
+function GM:PlayerLoadout( pl )
+	pl:Give( "weapon_pistol" )
+end
